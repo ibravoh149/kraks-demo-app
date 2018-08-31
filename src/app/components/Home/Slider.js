27642fa3a0b1c6b7acaef38d372e63d850afbdf1
ibrahim <ibravoh149@gmail.com>
2018-08-31 09:39:34 +0100

    bug fix

--- src/app/components/Home/Slider.js
+++ src/app/components/Home/Slider.js
@@ -14,7 +14,7 @@ const Slider = (props) => {
                     <a className="carousel-item red white-text" href="#one!">
                         <div >
                             {/* <img className="carouselImage" src='' alt="" /> */}
-                            <img className="carouselImage" src="https://cdn.kraks.co/upload/media/image/2018/05/1844/1527674347_logo.png" alt="" />
+                            <img className="carouselImage" src={img1} alt="" />
                         </div>
 
                         <div className="carouselContent">
@@ -39,7 +39,7 @@ const Slider = (props) => {
                     <a className="carousel-item red white-text" href="#one!">
                         <div className="carouselContainer">
                             {/* <img className="carouselImage" src="" alt="" /> */}
-                            <img className="carouselImage" src="https://cdn.kraks.co/upload/media/image/2018/05/1844/1527674347_logo.png" alt="" />
+                            <img className="carouselImage" src={img2} alt="" />
                         </div>
                         <div className="carouselContent">
                             <h2 className="carouselHeader">
