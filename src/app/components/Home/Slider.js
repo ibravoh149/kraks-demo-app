41e338efb207a85fa076d6494738a6aab7bbd5fd
ibrahim <ibravoh149@gmail.com>
2018-08-31 09:57:28 +0100

    bug fix

--- src/app/components/Home/Slider.js
+++ src/app/components/Home/Slider.js
@@ -13,8 +13,8 @@ const Slider = (props) => {
                 >
                     <a className="carousel-item red white-text" href="#one!">
                         <div >
-                            {/* <img className="carouselImage" src='' alt="" /> */}
-                            <img className="carouselImage" src={img1} alt="" />
+                            <img className="carouselImage" src='https://cdn.kraks.co/upload/media/image/2018/05/1844/1527674347_logo.png' alt="" />
+                            {/* <img className="carouselImage" src={img1} alt="" /> */}
                         </div>
 
                         <div className="carouselContent">
@@ -38,8 +38,8 @@ const Slider = (props) => {
 
                     <a className="carousel-item red white-text" href="#one!">
                         <div className="carouselContainer">
-                            {/* <img className="carouselImage" src="" alt="" /> */}
-                            <img className="carouselImage" src={img2} alt="" />
+                            <img className="carouselImage" src="https://cdn.kraks.co/upload/media/image/2018/05/1844/1527674347_logo.png" alt="" />
+                            {/* <img className="carouselImage" src={img2} alt="" /> */}
                         </div>
                         <div className="carouselContent">
                             <h2 className="carouselHeader">
--- src/app/index.js
+++ src/app/index.js
@@ -5,16 +5,6 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import  Routes from './Routes';
 
 
-// window.store= store;
-
-// ReactDOM.render(
-// <Provider store={store}>
-// <Router basename={process.env.PUBLIC_URL}>
-//     <Routes />
-// </Router>
-// </Provider>
-// , document.getElementById('root'));
-
 ReactDOM.render(
     <Router basename={process.env.PUBLIC_URL}>
         <Routes />
